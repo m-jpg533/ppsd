8f82e7ea6e2aefcb6ab79da433e6ee6844036d60
ming shi <115329011+m-jpg533@users.noreply.github.com>
2026-02-27 15:34:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-|-app.py
-|-templates
-     |-driver.html
-     |-map.html
-|-requirements.txt
-|-render.yaml
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Flask app is running successfully on Render!"
+
+@app.route("/driver")
+def driver():
+    return render_template("driver.html")
+
+@app.route("/map")
+def map_page():
+    return render_template("map.html")
+
+if __name__ == "__main__":
+    app.run()
